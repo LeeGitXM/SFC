82f07607bd0fcb35f898b945335a4e4bad4507f0
U-rforbes-laptoprforbes <rforbes@rforbes-laptop.(none)>
2014-10-09 14:25:16 -0600

    -n more sharing of constants

--- workspace/sfc-common/src/com/ils/sfc/common/IlsSfcNames.java
+++ workspace/sfc-common/src/com/ils/sfc/common/IlsSfcNames.java
@@ -3,20 +3,20 @@ package com.ils.sfc.common;
 /** A single place to define names that need to be shared between Java and Jython. */
 public class IlsSfcNames {
 
-	public static final String NAME = "name";
+	public static final String ACKNOWLEDGEMENT_REQUIRED = "ack. req'd";
 	public static final String DESCRIPTION = "description";
 	public static final String KEY = "key";
 	public static final String MESSAGE = "message";
+	public static final String MESSAGE_ID = "messageId";
+	public static final String NAME = "name";
+	public static final String POST_TO_QUEUE = "postToQueue";
+	public static final String PRIORITY = "priority";
 	public static final String PROMPT = "prompt";
 	public static final String QUEUE = "queue";
 	public static final String RECIPE_LOCATION = "recipeLocation";
+	public static final String RESPONSE_HANDLER = "sfcInputResponse";
 	public static final String STATUS = "status";
-	public static final String ACKNOWLEDGEMENT_REQUIRED = "ack. req'd";
-	public static final String PRIORITY = "priority";
-	public static final String MESSAGE_ID = "messageId";
-	public static final String POST_TO_QUEUE = "postToQueue";
 	public static final String TIMEOUT = "timeout";
 	public static final String TIMEOUT_UNIT = "timeoutUnit";
-	
 	public static final String TIME_UNIT_TYPE = "Time";
 }
