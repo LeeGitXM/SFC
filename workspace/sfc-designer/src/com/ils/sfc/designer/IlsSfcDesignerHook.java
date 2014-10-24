c959e9daea8caee9f6b0c100ea3f652df55ef1f5
Chuck Coughlin <ccoughlin@ils-automation.com>
2014-10-24 15:59:10 -0600

    Added prefuse jar, start of special browser.

--- workspace/sfc-designer/src/com/ils/sfc/designer/IlsSfcDesignerHook.java
+++ workspace/sfc-designer/src/com/ils/sfc/designer/IlsSfcDesignerHook.java
@@ -3,7 +3,30 @@
  */
 package com.ils.sfc.designer;
 
-import com.ils.sfc.client.step.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ils.sfc.client.step.AbortStepUI;
+import com.ils.sfc.client.step.ClearQueueStepUI;
+import com.ils.sfc.client.step.CollectDataStepUI;
+import com.ils.sfc.client.step.ControlPanelMessageStepUI;
+import com.ils.sfc.client.step.DebugPropertiesStepUI;
+import com.ils.sfc.client.step.DeleteDelayNotificationStepUI;
+import com.ils.sfc.client.step.DialogMessageStepUI;
+import com.ils.sfc.client.step.EnableDisableStepUI;
+import com.ils.sfc.client.step.IlsEnclosingStepUI;
+import com.ils.sfc.client.step.InputStepUI;
+import com.ils.sfc.client.step.LimitedInputStepUI;
+import com.ils.sfc.client.step.PauseStepUI;
+import com.ils.sfc.client.step.PostDelayNotificationStepUI;
+import com.ils.sfc.client.step.QueueMessageStepUI;
+import com.ils.sfc.client.step.RawQueryStepUI;
+import com.ils.sfc.client.step.SelectInputStepUI;
+import com.ils.sfc.client.step.SetQueueStepUI;
+import com.ils.sfc.client.step.ShowQueueStepUI;
+import com.ils.sfc.client.step.SimpleQueryStepUI;
+import com.ils.sfc.client.step.TimedDelayStepUI;
+import com.ils.sfc.client.step.YesNoStepUI;
 import com.ils.sfc.common.step.AbortStepProperties;
 import com.ils.sfc.common.step.ClearQueueStepProperties;
 import com.ils.sfc.common.step.CollectDataStepProperties;
@@ -24,6 +47,7 @@ import com.ils.sfc.common.step.ShowQueueStepProperties;
 import com.ils.sfc.common.step.SimpleQueryStepProperties;
 import com.ils.sfc.common.step.TimedDelayStepProperties;
 import com.ils.sfc.common.step.YesNoStepProperties;
+import com.ils.sfc.designer.browser.IlsBrowserFrame;
 import com.ils.sfc.util.IlsResponseManager;
 import com.ils.sfc.util.IlsSfcNames;
 import com.ils.sfc.util.PythonCall;
@@ -38,16 +62,31 @@ import com.inductiveautomation.sfc.SFCModule;
 import com.inductiveautomation.sfc.client.api.ClientStepRegistry;
 import com.inductiveautomation.sfc.client.api.ClientStepRegistryProvider;
 import com.inductiveautomation.sfc.designer.api.StepConfigRegistry;
-import com.inductiveautomation.sfc.elements.steps.enclosing.EnclosingStepProperties;
+import com.jidesoft.docking.DockContext;
+import com.jidesoft.docking.DockableFrame;
+import com.jidesoft.docking.DockingManager;
 
 public class IlsSfcDesignerHook extends AbstractDesignerModuleHook implements DesignerModuleHook {
 	private DesignerContext context = null;
 	private final LoggerEx log;
+	private IlsBrowserFrame browser = null;
 	
 	public IlsSfcDesignerHook() {
 		log = LogUtil.getLogger(getClass().getPackage().getName());
 	}
 		
+	@Override
+	public List<DockableFrame> getFrames() {
+		// Add a frame for our custom browser
+       	List<DockableFrame> frames = new ArrayList<>();
+       	browser = new IlsBrowserFrame(context);
+       	browser.setInitMode(DockContext.STATE_FRAMEDOCKED);
+       	browser.setInitSide(DockContext.DOCK_SIDE_WEST);
+       	browser.setInitIndex(2);
+       	frames.add(browser);
+       	return frames;
+	}
+	
 	@Override
 	public void initializeScriptManager(ScriptManager mgr) {
 		super.initializeScriptManager(mgr);
@@ -109,8 +148,12 @@ public class IlsSfcDesignerHook extends AbstractDesignerModuleHook implements De
        	configRegistry.register(SimpleQueryStepProperties.FACTORY_ID, editorFactory);
        	configRegistry.register(DebugPropertiesStepProperties.FACTORY_ID, editorFactory);
        	//configRegistry.register(EnclosingStepProperties.FACTORY_ID, editorFactory);
+       	
+       	
 	}
 		
+
+	
 	@Override
 	public void shutdown() {	
 	}
---
+++ workspace/sfc-designer/src/com/ils/sfc/designer/browser/IlsBrowserFrame.java
@@ -0,0 +1,56 @@
+package com.ils.sfc.designer.browser;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.inductiveautomation.ignition.common.util.LogUtil;
+import com.inductiveautomation.ignition.common.util.LoggerEx;
+import com.inductiveautomation.ignition.designer.gui.IconUtil;
+import com.inductiveautomation.ignition.designer.model.DesignerContext;
+import com.inductiveautomation.ignition.designer.model.ResourceWorkspaceFrame;
+import com.jidesoft.docking.DockableFrame;
+
+/** This is a container for a Perfuse TreeView,
+ * used to provide an alternative view of the list of SFC charts.
+ */
+public class IlsBrowserFrame extends DockableFrame implements ResourceWorkspaceFrame {
+	private static final long serialVersionUID = 4278524462387470494L;
+	private static final String TAG = "IlsBrowserFrame";
+	private static final String DOCKING_KEY = "SfcChartBrowserFrame";
+	private static final String TITLE = "Chart Browser";
+	private static final String SHORT_TITLE = "Charts";
+	private final DesignerContext context;
+	private final LoggerEx log = LogUtil.getLogger(getClass().getPackage().getName());
+	private final JPanel contentPanel;
+	
+	public IlsBrowserFrame(DesignerContext ctx) {
+		super(DOCKING_KEY);  // Pinned icon
+		context = ctx;
+		contentPanel = new JPanel(new BorderLayout());
+		init();
+	}
+
+	/**
+	 * Initialze the UI
+	 */
+	private void init() {
+		setTitle(TITLE);
+		setSideTitle(SHORT_TITLE);
+		setTabTitle(SHORT_TITLE);
+		setContentPane(contentPanel);
+		
+		contentPanel.setBorder(BorderFactory.createEtchedBorder());
+		JLabel marker = new JLabel("Hi Mom");
+		contentPanel.add(marker);
+	}
+	@Override
+	public String getKey() { return DOCKING_KEY; }
+	
+	@Override
+	public boolean isInitiallyVisible() {
+		return true;
+	}
+}
