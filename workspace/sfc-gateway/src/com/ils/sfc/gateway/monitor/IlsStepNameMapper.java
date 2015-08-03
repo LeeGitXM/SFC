165d28059e776fb0668d96bf1a21d8c826ee33d4
chuckcoughlin <chuck_coughlin@comcast.net>
2015-08-03 11:59:44 -0600

    Removed extra logging test message.

--- workspace/sfc-gateway/src/com/ils/sfc/gateway/monitor/IlsStepNameMapper.java
+++ workspace/sfc-gateway/src/com/ils/sfc/gateway/monitor/IlsStepNameMapper.java
@@ -95,7 +95,6 @@ public class IlsStepNameMapper {
 							// Use the chart-specific logger to announce its creation
 							LoggerEx logger = LogUtil.getLogger(path);
 							logger.infof("%s.initilize: Created chart resource and logger  %s",TAG,path);
-							log.infof("%s.initilize: Created chart resource  %s",TAG,path);
 							mapElement(path,res.getResourceId(),definition.getBeginElement());
 						}
 						else {
