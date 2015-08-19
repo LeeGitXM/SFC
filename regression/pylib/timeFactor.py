a8956e4d171b4e175acabd17e71a23d4339f6bbc
chuckcoughlin <chuck_coughlin@comcast.net>
2015-08-19 16:52:05 -0600

    Removed redundant pylib and references to it during build process.

--- regression/pylib/timeFactor.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2015. ILS Automation. All rights reserved.
-# Argument is the diagram path
-def setTimeFactor(common,factor):
-	import system.ils.blt.diagram as script
-	script.setTimeFactor(float(factor))
-
-def setSfcTimeFactor(common,factor):
-	import system.ils.sfc as ilssfc
-	ilssfc.setTimeFactor(float(factor))
