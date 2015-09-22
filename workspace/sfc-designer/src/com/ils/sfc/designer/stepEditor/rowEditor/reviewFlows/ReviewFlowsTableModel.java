4b8cca4e4bedb69d7b3a81e49d86191727adecc0
U-rforbes-laptop\rforbes <rforbes@rforbes-laptop.(none)>
2015-09-22 15:05:08 -0600

    Review Flows step works

--- workspace/sfc-designer/src/com/ils/sfc/designer/stepEditor/rowEditor/reviewFlows/ReviewFlowsTableModel.java
+++ workspace/sfc-designer/src/com/ils/sfc/designer/stepEditor/rowEditor/reviewFlows/ReviewFlowsTableModel.java
@@ -8,8 +8,8 @@ import com.ils.sfc.designer.stepEditor.rowEditor.RowTableModel;
 @SuppressWarnings("serial")
 public class ReviewFlowsTableModel extends RowTableModel {
 	public static final int DESTINATION_COLUMN = 4;
-	public static final int UNITS_COLUMN = 7;
-	private static final String[] columnNames = {"Config Key", "Flow 1", "Flow 2", "Total Flow", "Destination", "Prompt", "Advice", "Units"};
+	public static final int UNITS_COLUMN = 6;
+	private static final String[] columnNames = {"Config Key", "Flow 1", "Flow 2", "Total Flow", "Destination", "Prompt", "Units", "Advice"};
 
 	@Override
 	protected boolean isComboColumn(int col) {
