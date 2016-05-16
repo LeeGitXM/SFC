4811567810169de0b559fd5e4ef07892634cf567
U-rforbes-laptop\rforbes <rforbes@rforbes-laptop.(none)>
2016-05-16 16:16:38 -0600

    remove some print stmts

--- workspace/sfc-gateway/src/com/ils/sfc/step/IlsAbstractChartStep.java
+++ workspace/sfc-gateway/src/com/ils/sfc/step/IlsAbstractChartStep.java
@@ -96,7 +96,7 @@ public abstract class IlsAbstractChartStep extends AbstractChartElement<StepDefi
 	
 	@Override
 	public void activateStep(StepController controller) {
-		logger.info("Step activating");
+		//logger.info("Step activating");
 		// It is important for the correct function of Cancel steps that callPython be called
 		// once outside of the controller:execute stuff:
 		done = callPython(false);
@@ -109,26 +109,26 @@ public abstract class IlsAbstractChartStep extends AbstractChartElement<StepDefi
 
 	@Override
 	public void deactivateStep() {
-		logger.info("Step deactivated");
+		//logger.info("Step deactivated");
 		deactivated = true;
 		//In this example, we want to block flow until the work has finished. Therefore, we take no special action during deactivate.
 	}
 
 	@Override
 	public void cancelStep() {
-		logger.infof("Step cancelled");
+		//logger.infof("Step cancelled");
 		cancelled = true;
 	}
 
 	@Override
 	public void pauseStep() {
-		logger.infof("Example step paused.");
+		//logger.infof("Example step paused.");
 		paused = true;
 	}
 
 	@Override
 	public void resumeStep(StepController controller) {
-		logger.infof("Example step resumed.");
+		//logger.infof("Example step resumed.");
 		paused = false;
 		//On resume, we can just continue the work that we were previously doing.
 		controller.execute(this::doWork);
