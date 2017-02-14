cc13f15c91e52adb4c94b8d13dbadfc6e8ded7cf
chuckcoughlin <chuck_coughlin@comcast.net>
2017-02-13 21:42:14 -0700

    Polling for database S88 locator

--- workspace/sfc-gateway/src/com/ils/sfc/gateway/locator/S88Scope.java
+++ workspace/sfc-gateway/src/com/ils/sfc/gateway/locator/S88Scope.java
@@ -1,5 +1,10 @@
 package com.ils.sfc.gateway.locator;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.ils.common.JavaToPython;
 import com.ils.sfc.common.PythonCall;
 import com.inductiveautomation.ignition.common.util.LogUtil;
 import com.inductiveautomation.ignition.common.util.LoggerEx;
@@ -15,6 +20,9 @@ public class S88Scope extends PyChartScope {
 	private final PyChartScope chartScope;
 	private final String identifier;
 	private final String fullKey;
+	private final JavaToPython j2p = new JavaToPython();
+	private final S88PollTask pollTask;
+	private final ScheduledExecutorService executor;
 	
 	public S88Scope(PyChartScope chartScope,PyChartScope stepScope,String identifier, String fullKey) {
 		this.chartScope = chartScope;
@@ -22,17 +30,24 @@ public class S88Scope extends PyChartScope {
 		this.identifier = identifier;
 		this.fullKey = fullKey;
 		log.infof("Instantiating my very own S88Scope object");
+        this.pollTask = new S88PollTask(chartScope,stepScope,fullKey);
+        this.executor = Executors.newSingleThreadScheduledExecutor();
 	}
 
-	
+	/**
+	 * When there are no more observers, shut down the executor.
+	 */
 	@Override
 	public void removeScopeObserver(ScopeObserver observer) {
-
+		super.removeScopeObserver(observer);
+		if( observers.isEmpty() ) {
+			executor.shutdown();
+		}
 	}
 	
 	@Override
 	public boolean containsKey(Object key) {
-		log.infof("HELLO Key: %s", key.toString());
+		log.infof("CONTAINS KEY? %s", key.toString());
 		return true;
 	}
 		
@@ -45,29 +60,61 @@ public class S88Scope extends PyChartScope {
 	 *  subfolders as the keys.
 	 */
 	public Object get(Object keyObj) {		
-		// Build the tag path as far as the name of the UDT
+		// Build the key path one section at a time.  A key with only a single element is illegal.
 		String key = keyObj.toString();
-		if (!fullKey.isEmpty()) {
+		if( !fullKey.isEmpty()) {
 			key = fullKey + "." + key;
-		}
-		Object result = "ERROR";
-		try{ 
-			log.infof("Key: %s", key);
-			log.infof("Identifier: %s", identifier);
-			
-			result = PythonCall.S88_GET.exec(chartScope,stepScope,key,identifier);
-			log.infof("****  S88Get worked ****");
-			return new PyChartScope();
-			
+			try{ 
+				log.infof("Key: %s", key);
+				log.infof("Identifier: %s", identifier);
 
+				Object value  = PythonCall.S88_GET.exec(chartScope,stepScope,key,identifier);
+				log.infof("****  S88Get worked ****");
+				pollTask.setKey(key);
+				executor.scheduleAtFixedRate(pollTask, 5, 3, TimeUnit.SECONDS);  // Every 5 seconds
+				PyChartScope result = new PyChartScope();
+				result.put(key, value);
+				return result;
+			}
+			catch(Exception ex) {
+				log.errorf("EXCEPTION: Fetching %s (%s)", key, ex.getMessage());
+			}
 		}
-		catch(Exception ex) {
-			log.errorf("EXCEPTION: %s", ex.getMessage());
-			
-		}
-		log.infof("The key wasn't valid, returning a s88Scope");
 		return new S88Scope(chartScope, stepScope, identifier, key);
 	}
-
+	//================================= Execution Task ========================================
+	/** 
+	 * Run periodically to read the recipe data value..
+	 */
+	private class S88PollTask implements Runnable{
+		private String key = "";
+		private final PyChartScope chartScope;
+		private final PyChartScope stepScope;
+		private final String identifier;
+		/**
+		 * Constructor 
+		 */
+		public S88PollTask(PyChartScope cScope,PyChartScope sScope,String ident) {
+			this.chartScope = cScope;
+			this.stepScope  = sScope;
+			this.identifier = ident;
+		}
+		
+		public void setKey(String fullkey) { this.key=fullkey; }
+		/**
+		 * Execute a single read.
+		 */
+		public void run() {
+			try {
+				Object value  = PythonCall.S88_GET.exec(chartScope,stepScope,key,identifier);
+				for(ScopeObserver observer:observers) {
+					observer.observe(key, j2p.objectToPy(value));
+				}
+			}
+			catch(Exception ex) {
+				log.errorf("EXCEPTION: Running S88 %s (%s)", key, ex.getMessage());
+			}
+		}
+	}
 }
 
