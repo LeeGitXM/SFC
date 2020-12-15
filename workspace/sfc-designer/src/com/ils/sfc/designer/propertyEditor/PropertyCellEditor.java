ca2950b82337f8553763c9d1c6ec472e0be43cb6
Pete-asus <phassler@ils-automation.com>
2020-12-14 20:59:28 -0600

    Ticket #758 - Set the current value for the combo box.  Fixes bug where selecting the combo box always selects the first value.

--- workspace/sfc-designer/src/com/ils/sfc/designer/propertyEditor/PropertyCellEditor.java
+++ workspace/sfc-designer/src/com/ils/sfc/designer/propertyEditor/PropertyCellEditor.java
@@ -1,6 +1,5 @@
 package com.ils.sfc.designer.propertyEditor;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,26 +9,40 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellEditor;
 
 import com.ils.sfc.designer.DesignerUtil;
+import com.inductiveautomation.ignition.common.config.PropertyValue;
+import com.inductiveautomation.ignition.common.util.LogUtil;
+import com.inductiveautomation.ignition.common.util.LoggerEx;
 import com.inductiveautomation.sfc.designer.workspace.editors.ChartPathComboBox;
 import com.inductiveautomation.sfc.elements.steps.enclosing.EnclosingStepProperties;
 
-/** A table cell editor for the property grid */
+/***
+ * This is used for the primary property editor for a step.
+ * It is not used for a secondary property editor. 
+ */
+
 @SuppressWarnings("serial")
 class PropertyCellEditor extends AbstractCellEditor implements TableCellEditor{
 	private Component component;
 	private PropertyCellComponentFactory factory = new PropertyCellComponentFactory();
+	private final LoggerEx log = LogUtil.getLogger(getClass().getName());
 
 	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected,
 		int row, int col) {
 		PropertyTableModel model = (PropertyTableModel) table.getModel();
 		PropertyRow rowObj = ((PropertyTableModel) table.getModel()).getRowObject(row);
 		if(rowObj.getChoices() != null) {
+			log.tracef("Making a combo box...");
+			PropertyValue pv = rowObj.getPropertyValue();
+
+			log.tracef("...the current value is: %s", pv.getValue().toString());
+			
 			JComboBox<Object> combo = new JComboBox<Object>(rowObj.getChoices());
+
 			combo.setBackground(java.awt.Color.white);
+			combo.setSelectedItem(pv.getValue().toString());
 			combo.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					fireEditingStopped();
